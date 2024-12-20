96350bae0f51a85a6bfbb80aba26dec131f103af
Nisha <nisha.bioinfo@gmail.com>
2024-12-20 15:41:03 +0100

    easy toggle btn react problem

--- day-02/practice1.js
+++ day-02/practice1.js
@@ -506,3 +506,31 @@ function LongestWord(sen) {
      
   // keep this function call here 
   console.log(LongestWord(readline()));
+
+
+
+//   React Button Toggle
+// We provided some simple React template code. Your goal is to modify the component so that you can properly toggle the button to switch between an ON state and an OFF state. When the button is on and it is clicked, it turns off and the text within it changes from ON to OFF and vice versa. Make use of component state for this challenge.
+
+// You are free to add classes and styles, but make sure you leave the component ID's and classes provided as they are. Submit your code once it is complete and our system will validate your output.
+
+
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function Toggle() {
+const [buttonState, setButtonState] = useState(true);
+  function handleClick() {
+    setButtonState(!buttonState);
+
+    // todo
+  }
+  
+  return (
+    <button onClick={handleClick}> {buttonState ? "ON" : "OFF"}</button>
+  );
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Toggle />);
