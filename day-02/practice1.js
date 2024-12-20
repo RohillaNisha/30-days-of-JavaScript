4dc4d172dba2dba3b9c597185d1280f2bc957691
Nisha <nisha.bioinfo@gmail.com>
2024-12-20 15:38:24 +0100

    first factorial easy js problem

--- day-02/practice1.js
+++ day-02/practice1.js
@@ -449,3 +449,28 @@ function FirstReverse(str) {
      
   // keep this function call here 
   console.log(FirstReverse(readline()));
+
+
+//   First Factorial
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+// Examples
+// Input: 4
+// Output: 24
+// Input: 8
+// Output: 40320
+
+
+
+function FirstFactorial(num) { 
+
+    // code goes here   
+    for(let i= num-1; i >=1 ; i--){
+      num = num * i
+    }
+  
+    return num; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(FirstFactorial(readline()));
