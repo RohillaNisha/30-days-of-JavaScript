81982b01a56a799b0664e2269859b29494e7c533
Nisha <nisha.bioinfo@gmail.com>
2024-12-20 15:35:47 +0100

    reverse the word js easy problem from coderbyte

--- day-02/practice1.js
+++ day-02/practice1.js
@@ -422,3 +422,30 @@ function QuestionsMarks(str) {
        
     // keep this function call here 
     console.log(QuestionsMarks(readline()));
+
+
+//     First Reverse
+// Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+// Examples
+// Input: "coderbyte"
+// Output: etybredoc
+// Input: "I Love Code"
+// Output: edoC evoL I
+
+
+function FirstReverse(str) { 
+
+    let outputStringArray = [];
+  
+    for(i=str.length-1 ; i>=0 ; i--){
+      outputStringArray.push(str.charAt(i));
+    }
+  
+  
+    // code goes here  
+    return outputStringArray.join(""); 
+  
+  }
+     
+  // keep this function call here 
+  console.log(FirstReverse(readline()));
